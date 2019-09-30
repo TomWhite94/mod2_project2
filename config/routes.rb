ae90405438843d7385dd051b293d957e44a981dd
github username <github email address>
2019-10-01 00:57:46 +0100

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ resources :users
 resources :sessions
 
 
-  
 get "/quizzes", to: "quizzes#index", as: "quizzes"
-get "/quizzes/sort", to: "quizzes#sort", as: "sort" # receieve difficulty and genre
-get "/quizzes/sort/main", to: "quizzes#call" # call data from the database and freturn
 get "/quizzes/quiz_session", to: "quizzes#quiz_session", as: "quiz_session"
 
 end
