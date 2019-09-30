c2ff650cda74fa41c39c4aa206fd26a9c2a1fec5
github username <github email address>
2019-09-30 18:58:58 +0100

    A

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def show
+    end
+
+    def create
+        @user = User.new(user_params)
+        @user.save
+        session[:user_id] = @userid
+        
+        redirect_to quizzes_path
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+
+
+
+    private
+
+    def user_params
+        params.require(:user).permit!
+    end
 end
