c85c25efda9fb73b24460e45ca3c5b3690e1d265
github username <github email address>
2019-10-01 09:24:21 +0100

    whatever

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,34 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @user = User.find_by(name: params[:user][:name])
+
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to quizzes_path
+        else 
+            flash[:notice] = "Sorry, we can't find a user with that login"
+            redirect_to new_session_path
+        end
+    end
+
+
+    def destroy 
+        session.destroy
+        redirect_to new_session_path
+    end
+
+    private
+    
+    def session_params
+        params.require(:sessions).permit!
+    end
+
+
+
+
+
+end
